e25ba88ae9216d40f871d6b85556a1f8171d19aa
TroelsM <Troels@bj59.dk>
2017-09-15 11:12:28 +0200

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -3,7 +3,7 @@ session_start();
 
 $DB_HOST = 'troelsm.com.mysql';
 $DB_USER = 'troelsm_com';
-$DB_PASS = 'JdySGyTF';
+$DB_PASS = '';
 $DB_NAME = 'troelsm_com';
 
 $link = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
